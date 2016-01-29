e98715206bd4f9e7c867e412f08d29937f7deac8
Thomas Gebert <thomas@gebert.sexy>
2016-01-29 14:14:40 -0500

    Get a basic list rendering to the screen

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import ChatBox from "./ChatBoxReducer"
+import React from 'react';
 import {createStore, applyMiddleware} from 'redux';
-
+import ReactDOM from 'react-dom';
 let store = createStore(ChatBox);
 console.log("Hello!",  ChatBox);
 setInterval(() =>
             store.dispatch({type:"ADD_MESSAGE", message: "Hello hello"})
             , 1000);
 
-store.subscribe(() => console.log("State:", store.getState().first(), store.getState().last()))
+const Chat = React.createClass({
+  render:function(){
+    return ( <div>
+             {this.props.messages.map( (message) => <div>{message}</div>)}
+             </div>)
+  }
+});
+
+const render = () => {
+  ReactDOM.render(
+      <Chat messages={store.getState()}/>, document.getElementById("app")
+  );
+}
+
+store.subscribe(render)
+
+
