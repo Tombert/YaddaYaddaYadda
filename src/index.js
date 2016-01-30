f21d19111f73c3b4b387894385796fe85d3a4a9a
Thomas Gebert <thomas@gebert.sexy>
2016-01-29 19:05:58 -0500

    We have a working chat server.  Just node right now, probably will convert to Erlang.

--- src/index.js
+++ src/index.js
@@ -4,13 +4,20 @@ import ReactDOM from 'react-dom';
 import Chat from "./ChatComponent"
 import React from 'react';
 import TextInput from "./TextComponent";
+import io from "socket.io-client";
 
 let store = createStore(ChatBox);
 
+let socket = io("localhost:3000");
 const send = (value) =>{
-  store.dispatch({type: "ADD_MESSAGE", message: value}); 
+  socket.emit("chat message", value);
+//  store.dispatch({type: "ADD_MESSAGE", message: value}); 
 }; 
 
+
+socket.on("chat message", (message) =>{
+  store.dispatch({type: "ADD_MESSAGE", message}); 
+});
 const render = () => {
   ReactDOM.render(
     (<div>
@@ -26,5 +33,8 @@ const render = () => {
 
 store.subscribe(render)
 
-store.dispatch({type:"blah"});
+
+
+
+render();
 
